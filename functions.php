ee0368857ed9ecc453d07b1fd123ed4fef06d098
mateuswetah <mateus.m.luna@gmail.com>
2020-04-03 18:35:50 -0300

    Adds v-libras plugin.

--- functions.php
+++ functions.php
@@ -20,3 +20,28 @@ function veganos_parent_theme_enqueue_styles() {
 	);
 
 }
+
+function tainacan_unb_covid_19_scripts() {
+
+	wp_register_script( 'v_libras_library', 'https://vlibras.gov.br/app/vlibras-plugin.js', [] );
+	wp_enqueue_script( 'v_libras_library' );
+
+	wp_register_script( 'v_libras_script', get_stylesheet_directory_uri() . '/assets/js/v-libras.js', []);
+	wp_enqueue_script( 'v_libras_script' );
+
+}
+add_action( 'wp_enqueue_scripts', 'tainacan_unb_covid_19_scripts' );  
+
+function tainacan_unb_covid_19_sv_libras_tags() {
+	?>
+		<div vw class="enabled">
+			<div vw-access-button class="active"></div>
+			<div vw-plugin-wrapper>
+				<div class="vw-plugin-top-wrapper"></div>
+			</div>
+		</div>
+	<?php
+}
+add_action('wp_footer', 'tainacan_unb_covid_19_sv_libras_tags');
+
+?>
