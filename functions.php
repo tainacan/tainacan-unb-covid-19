111e30dd536b326046e1928c0ce24c30f663efd6
mateuswetah <mateus.m.luna@gmail.com>
2020-04-15 10:00:02 -0300

    Adiciona a barra-brasil e remove o v-libras.

--- functions.php
+++ functions.php
@@ -23,15 +23,18 @@ function veganos_parent_theme_enqueue_styles() {
 
 function tainacan_unb_covid_19_scripts() {
 
+	wp_register_script( 'barra_brasil', 'https://barra.brasil.gov.br/barra.js', [], false, true );
+	wp_enqueue_script( 'barra_brasil' );
+/*
 	wp_register_script( 'v_libras_library', 'https://vlibras.gov.br/app/vlibras-plugin.js', [] );
 	wp_enqueue_script( 'v_libras_library' );
 
 	wp_register_script( 'v_libras_script', get_stylesheet_directory_uri() . '/assets/js/v-libras.js', []);
 	wp_enqueue_script( 'v_libras_script' );
-
+*/
 }
 add_action( 'wp_enqueue_scripts', 'tainacan_unb_covid_19_scripts' );  
-
+/*
 function tainacan_unb_covid_19_sv_libras_tags() {
 	?>
 		<div vw class="enabled">
@@ -43,5 +46,21 @@ function tainacan_unb_covid_19_sv_libras_tags() {
 	<?php
 }
 add_action('wp_footer', 'tainacan_unb_covid_19_sv_libras_tags');
+*/
+function tainacan_unb_covid_19_barra_brasil_template() {
+	?>
+		<div id="barra-brasil" style="background:#7F7F7F; height: 20px; padding:0 0 0 10px;display:block;">
+			<ul id="menu-barra-temp" style="list-style:none;">
+				<li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED">
+					<a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>
+				</li>
+				<li>
+				<a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a>
+				</li>
+			</ul>
+		</div>
+	<?php
+}
+add_action('wp_head', 'tainacan_unb_covid_19_barra_brasil_template');
 
 ?>
