280ad5f22af4c6b1cd7fde3278493424ac2e89c3
mateuswetah <mateus.m.luna@gmail.com>
2020-04-16 10:45:27 -0300

    Remove v-libras conflitante com o da barra brasil.

--- assets/js/v-libras.js
+++
@@ -1,1 +0,0 @@
-new window.VLibras.Widget('https://vlibras.gov.br/app');
--- functions.php
+++ functions.php
@@ -28,27 +28,8 @@ function tainacan_unb_covid_19_scripts() {
 
 	wp_register_script( 'barra_unb', get_stylesheet_directory_uri() . '/assets/js/barra-unb.js', []);
 	wp_enqueue_script( 'barra_unb' );
-
-	wp_register_script( 'v_libras_library', 'https://vlibras.gov.br/app/vlibras-plugin.js', [] );
-	wp_enqueue_script( 'v_libras_library' );
-
-	wp_register_script( 'v_libras_script', get_stylesheet_directory_uri() . '/assets/js/v-libras.js', []);
-	wp_enqueue_script( 'v_libras_script' );
-
 }
 add_action( 'wp_enqueue_scripts', 'tainacan_unb_covid_19_scripts' );  
-
-function tainacan_unb_covid_19_sv_libras_tags() {
-	?>
-		<div vw class="enabled">
-			<div vw-access-button class="active"></div>
-			<div vw-plugin-wrapper>
-				<div class="vw-plugin-top-wrapper"></div>
-			</div>
-		</div>
-	<?php
-}
-add_action('wp_footer', 'tainacan_unb_covid_19_sv_libras_tags');
 
 function tainacan_unb_covid_19_barra_brasil_template() {
 	?>
